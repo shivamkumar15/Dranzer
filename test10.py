2515bc755edea0da18c3678c7074e435ad8324e2
shivamkumar15 <shivamk4025@gmail.com>
2026-04-17 23:07:42 +0530

    Removed useless files

--- test10.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-from bs4 import BeautifulSoup
-url = "https://wiki.hyprland.org/Configuring/Variables/"
-req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-html = urllib.request.urlopen(req).read()
-soup = BeautifulSoup(html, 'html.parser')
-tags = soup.find_all(text=lambda x: x and 'workspace_swipe' in x.lower())
-for t in tags:
-    print(t.strip())
--- test14.py
+++
@@ -1,8 +0,0 @@
-import urllib.request
-from bs4 import BeautifulSoup
-url = "https://wiki.hyprland.org/Configuring/Variables/"
-req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-html = urllib.request.urlopen(req).read()
-soup = BeautifulSoup(html, 'html.parser')
-for t in soup.find_all(string=lambda x: x and 'new gestures system' in x.lower()):
-    print(t.parent.parent.get_text())
